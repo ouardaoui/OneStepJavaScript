8302c6cf8498057b6cad37a9266be8fc8a2b81db
lghrab <59561825+lghrab@users.noreply.github.com>
2020-01-22 23:13:11 +0100

    .

---
+++ lastarr1.js
@@ -0,0 +1,34 @@
+const account ={
+    name:"ayoub ouardaoui",
+    expense:[],
+    Income:[],
+    addExpense :function (Description,amount){
+        this.expense.push({
+            Description:Description,
+            amount:amount
+        })
+    },
+    addIncome :function (Description,amount){
+        this.Income.push({
+            Description:Description,
+            amount:amount
+        })
+    },
+    summary:function(){
+        let totalexpense =0
+        let totalIncome=0
+        let total=0
+        this.expense.forEach(function(expense){
+            totalexpense= totalexpense+expense.amount
+        })
+        this.Income.forEach(function(Income){
+            totalIncome= totalIncome+Income.amount
+        })
+        total =totalIncome-totalexpense
+        return this.name+" has "+total+" $."
+    }
+}
+account.addExpense("Rent",950)
+account.addExpense("Coffee",2)
+account.addIncome("job",1000)
+console.log(account.summary())
