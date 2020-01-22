aab02a71cd40ac9ee210c979af84a1c28d855a45
lghrab <59561825+lghrab@users.noreply.github.com>
2020-01-22 16:10:10 +0100

    hi

---
+++ arr-obj.js
@@ -0,0 +1,15 @@
+const note=[{
+    title:"My next trip",
+    body:"I would like to go to spain"
+},{
+    title:"Hobbits to work on",
+    body:"Exercise,Eatiing a bit better"
+},{
+    title:"Office modificatuion",
+    body:"Get a new seat"
+}]
+note.push({nothing:"hello"})
+console.log(note)
+a={}
+b=a
+console.log(a===b)
