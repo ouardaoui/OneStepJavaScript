3f8a57b7547810fe210ad9717a78a5090ed436a4
ouardaoui <ayoubouardaoui3@gmail.com>
2021-09-28 00:16:13 +0100

    it s fun

---
+++ hooks/ninja2/src/App1.js
@@ -0,0 +1,9 @@
+import Counter from "./componenet/reducer";
+
+const App1 = () => {
+    return ( 
+        <Counter/>
+     );
+}
+ 
+export default App1;
--- hooks/ninja2/src/index.js
+++ hooks/ninja2/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App1 from './App1';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App1 />
   </React.StrictMode>,
   document.getElementById('root')
 );
