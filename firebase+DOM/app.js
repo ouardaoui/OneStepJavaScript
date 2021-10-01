38e4ce9cc6d7bea6970563fc5d6a009e5adf05f8
ouardaoui <ayoubouardaoui3@gmail.com>
2021-10-01 19:20:57 +0100

    finaly

---
+++ firebase+DOM/app.js
@@ -0,0 +1,11 @@
+
+
+var citiesRef = db.collection("ragnar");
+
+citiesRef.doc("SF").set({
+    name: "San Francisco", state: "CA", country: "USA",
+    capital: false, population: 860000,
+    regions: ["west_coast", "norcal"] });
+
+
+
