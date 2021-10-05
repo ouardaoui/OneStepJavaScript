68fc7ea10717e0ddeae4e7a0d2067b41615efb0c
ouardaoui <ayoubouardaoui3@gmail.com>
2021-10-05 22:55:39 +0100

    firebase+flsak +react

--- API/index.js
+++ API/index.js
@@ -1,17 +1,17 @@
 const axios = require('axios')
 
 // axios promise based 
-axios({
-    url : "/todos/3",
-    baseURL : "https://jsonplaceholder.typicode.com",
-    method :"get"
-}).then((response) => {
-    console.log(response.data)
-}).catch((err) =>{
-    console.log(err)
-}).then(() =>
-console.log("end http requist") //always excuted
-)
+// axios({
+//     url : "/todos/3",
+//     baseURL : "https://jsonplaceholder.typicode.com",
+//     method :"get"
+// }).then((response) => {
+//     console.log(response.data)
+// }).catch((err) =>{
+//     console.log(err)
+// }).then(() =>
+// console.log("end http requist") //always excuted
+// )
 
 // Response Get with params 
 
@@ -75,17 +75,23 @@ console.log("end http requist") //always excuted
 // console.log("get start ...")
 
 // Global axios default axios.defaults.baseURL
-const todos = axios.create()
-todos.defaults.baseURL = "https://jsonplaceholder.typicode.com"
+// const todos = axios.create()
+// todos.defaults.baseURL = "https://jsonplaceholder.typicode.com"
 
-const getData = async () =>{
-    try {
-        const response = await todos.get("/todos/100")
-        console.log(response.data)
-    }catch(e){
-        console.log(err)
-    }
-}
-getData()
-console.log("get start ...")
+// const getData = async () =>{
+//     try {
+//         const response = await todos.get("/todos/100")
+//         console.log(response.data)
+//     }catch(e){
+//         console.log(err)
+//     }
+// }
+// getData()
+// console.log("get start ...")
 
+
+// axios promise based 
+axios.get("http://localhost:5000/members"
+).then((response) => {
+    console.log(response.data)
+})
--- reactjs/portfolio/src/App.js
+++
@@ -1,9 +0,0 @@
-import Approuter from "./approuters/Approuter";
-
-function App() {
-  return (
-    <Approuter/>
-  );
-}
-
-export default App;
